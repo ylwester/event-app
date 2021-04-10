1f276bd653bbed8484b4d7684a5e35983f60d807
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-04-10 19:39:31 +0200

    styling navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Route, Switch,} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import BrowseEvents from "./components/BrowseEvents.component";
 import HomePage from "./components/HomePage.component";
@@ -10,23 +10,23 @@ import LoginPage from "./components/LoginPage.component";
 import RegisterPage from "./components/RegisterPage.component";
 import Navigation from "./components/Navigation.component";
 
-
-
 function App() {
-    return (
-        <div>
-            <Navigation/>
-            <Switch>
-                <Route path="/" exact component={HomePage}/>
-                <Route path="/event/add" component={AddEventItem}/>
-                <Route path="/contact" component={ContactPage}/>
-                <Route path="/login" component={LoginPage}/>
-                <Route path="/register" component={RegisterPage}/>
-                <Route path="/events"> <BrowseEvents/></Route>
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/event/add" component={AddEventItem} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/events">
+          {" "}
+          <BrowseEvents />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
-
 
 export default App;
--- client/src/components/Navigation.component.js
+++ client/src/components/Navigation.component.js
@@ -1,37 +1,51 @@
-import {useState} from "react";
+import { useState } from "react";
 import {
-    Collapse,
-    Nav,
-    Navbar,
-    NavbarBrand,
-    NavbarToggler,
-    NavItem,
-    NavLink
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
 } from "reactstrap";
 
 const Navigation = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    return (
-        <div>
-            <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">EVENT APP</NavbarBrand>
-                <NavbarToggler onClick={toggle}/>
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/events">Przeglądaj</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">EVENT APP</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/event/add">Dodaj wydarzenie</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/events">Przeglądaj</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Zaloguj się
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                <NavLink href="/login">Zaloguj się</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/register">Zarejestruj</NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
 
-
-    );
-
-}
-
-
-export default Navigation
+export default Navigation;
