f992121538ff432c8d15a1fd6ef410a3e5ebc359
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-05-03 17:16:10 +0200

    event list item display styling

--- client/src/components/EventListItem.component.js
+++ client/src/components/EventListItem.component.js
@@ -4,22 +4,31 @@ import "../styles/eventListItem.css";
 import {Link} from "react-router-dom";
 
 const EventListItem = ({event}) => {
+
+    function convertData(data) {
+        let dataArray = data.split("-");
+        return dataArray[2]+"."+dataArray[1]+"."+dataArray[0];
+    }
+
     return (
         <Card style={{width: "35rem"}}>
-            <CardBody style={{padding: "11px"}}>
+            <CardBody style={{padding: "10px"}}>
                 <header>
                     <div className="title">{event.title}</div>
                     <div className="price">{event.paid ? event.price + "zł" : "Darmowe"}</div>
                 </header>
+                <div id="middle-wrapper">
+                    <div className="event-data">{convertData(event.day)}{event.time ? ", " + event.time : null}</div>
+                    <div id="location">
+                        {event.city}
+                    </div>
+                </div>
                 <div id="container">
                     <div id="left-wrapper">
                         <div className="categories">
                             {event.categories.map(category =>
                                 <div className="cat" key={category.id}>{category.name}</div>
                             )}
-                        </div>
-                        <div id="location">
-                             {event.city}
                         </div>
                     </div>
                     <div id="full-post">
