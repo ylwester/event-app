3d2719abaea5a205c435225424584e594ff1a0ae
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-05-10 20:56:51 +0200

    register form

--- client/src/components/RegisterPage.component.js
+++ client/src/components/RegisterPage.component.js
@@ -1,6 +1,40 @@
+import {Button, Col, Form, FormGroup, Input, Label} from "reactstrap";
+
 const RegisterPage = () => {
+
+
+
     return (
-        <h1>Register page</h1>
+        <div style={{margin: "auto", width: "50vh"}}>
+            <h1>Zarejestruj się</h1>
+            <Form>
+                <FormGroup row>
+                    <Label sm={3} for="userName">Username</Label>
+                    <Col sm={8}>
+                        <Input type="text" name="username" id="userName" placeholder="Podaj swój nick" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label sm={3} for="email">Email</Label>
+                    <Col sm={8}>
+                        <Input type="email" name="email" id="email" placeholder="Podaj swój email" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label sm={3} for="password">Hasło</Label>
+                    <Col sm={8}>
+                        <Input type="password" name="password" id="password" placeholder="Podaj hasło" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label sm={3} for="email">Potwierdź hasło</Label>
+                    <Col sm={8}>
+                        <Input type="password" name="confirmPassword" id="confirmPassword" placeholder="Potwierdź hasło" />
+                    </Col>
+                </FormGroup>
+                <Button>Zatwierdź</Button>
+            </Form>
+        </div>
     )
 }
 
