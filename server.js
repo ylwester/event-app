52cce5727a4908888eedc315e571a7a2ae970b46
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-06-06 22:55:24 +0200

    Version 1.0

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const eventItems = require("./routes/api/eventItems");
 const userAuth = require("./routes/api/userAuth");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require('path')
 
 const app = express();
 const PORT = 5000;
@@ -22,6 +23,14 @@ mongoose
   .catch((err) => console.log(err));
 
 mongoose.set('useFindAndModify', false);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.use(cookieParser());
 
