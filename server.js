c62d005bd5dac974a969fdd19ae75beb655af0e9
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-06-07 15:52:43 +0200

    make it better

--- server.js
+++ server.js
@@ -66,13 +66,13 @@ app.use(routes);
 //     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     })
+// }
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello</h1>`);
